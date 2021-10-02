af0d9e53ca754f020eaf8f8073c79f31371b6616
sappiamnl <manuele.sappia@gmail.com>
2021-10-02 16:14:00 +0200

    3. Styled Components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,33 +1,65 @@
 import React from "react";
 import home1 from "../img/home1.png";
+//styled
+import styled from "styled-components";
 
 const AboutSection = () => {
   return (
-    <div>
-      <div className="description">
+    <About>
+      <Description>
         <div className="title">
-          <div className="hide">
+          <Hide>
             <h2>we work to make</h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>
               your <span>dreams</span> come
             </h2>
-          </div>
-          <div className="hide">
+          </Hide>
+          <Hide>
             <h2>true</h2>
-          </div>
+          </Hide>
         </div>
         <p>
           Contattaci per qualsiasi idea fotografica o cinematografica, siamo dei
           professionisti affermati nel mestiere.
         </p>
         <button>Contact Us</button>
-      </div>
-      <div className="img">
+      </Description>
+      <Image>
         <img src={home1} alt="profilo" />
-      </div>
-    </div>
+      </Image>
+    </About>
   );
 };
+
+//styled components
+const About = styled.div`
+  min-height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 5rem 10rem;
+  color: white;
+`;
+const Description = styled.div`
+  flex: 1;
+  padding-right: 5rem;
+  h2 {
+    font-weight: lighter;
+  }
+`;
+const Image = styled.div`
+  flex: 1;
+  overflow: hidden;
+  img {
+    width: 100%;
+    height: 80vh;
+    object-fit: cover;
+  }
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
 export default AboutSection;
