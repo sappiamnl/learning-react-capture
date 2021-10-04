067338494fce856ab6d017d603cf2e1c6bab9d0f
sappiamnl <manuele.sappia@gmail.com>
2021-10-04 22:36:40 +0200

    Wave animation ang page transition

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,6 +4,8 @@ import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../styles";
 //Framer Motion
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
   return (
@@ -11,26 +13,27 @@ const AboutSection = () => {
       <Description>
         <motion.div>
           <Hide>
-            <motion.h2>we work to make</motion.h2>
+            <motion.h2 variants={titleAnim}>we work to make</motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true</motion.h2>
+            <motion.h2 variants={titleAnim}>true</motion.h2>
           </Hide>
         </motion.div>
-        <p>
+        <motion.p variants={fade}>
           Contattaci per qualsiasi idea fotografica o cinematografica, siamo dei
           professionisti affermati nel mestiere.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contattaci</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="profilo" />
+        <motion.img variants={photoAnim} src={home1} alt="profilo" />
       </Image>
+      <Wave />
     </About>
   );
 };
--- src/styles.js
+++ src/styles.js
@@ -12,13 +12,16 @@ export const About = styled.div`
 export const Description = styled.div`
   flex: 1;
   padding-right: 5rem;
+  z-index: 2;
   h2 {
     font-weight: lighter;
+    font-size: 3.6rem;
   }
 `;
 export const Image = styled.div`
   flex: 1;
   overflow: hidden;
+  z-index: 2;
   img {
     width: 100%;
     height: 80vh;
